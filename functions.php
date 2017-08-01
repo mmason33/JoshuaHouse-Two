9fbe0dd843b72e7d91fd4bdc9dcccf12caaaddac
michael mason <michael@michaeljmason.com>
2017-08-01 15:32:26 -0700

    init

---
+++ functions.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Sage includes
+ *
+ * The $sage_includes array determines the code library included in your theme.
+ * Add or remove files to the array as needed. Supports child theme overrides.
+ *
+ * Please note that missing files will produce a fatal error.
+ *
+ * @link https://github.com/roots/sage/pull/1042
+ */
+$sage_includes = [
+  'lib/assets.php',    // Scripts and stylesheets
+  'lib/extras.php',    // Custom functions
+  'lib/setup.php',     // Theme setup
+  'lib/titles.php',    // Page titles
+  'lib/wrapper.php',   // Theme wrapper class
+  'lib/customizer.php' // Theme customizer
+];
+
+foreach ($sage_includes as $file) {
+  if (!$filepath = locate_template($file)) {
+    trigger_error(sprintf(__('Error locating %s for inclusion', 'sage'), $file), E_USER_ERROR);
+  }
+
+  require_once $filepath;
+}
+unset($file, $filepath);
+
+
+if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
+function my_jquery_enqueue() {
+   wp_deregister_script('jquery');
+   wp_deregister_script( 'wp-embed' );
+   wp_register_script('jquery', "//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js", false, null);
+   wp_enqueue_script('jquery');
+}
+
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+
+function remove_recent_comments_style() {
+    global $wp_widget_factory;
+    remove_action('wp_head', array($wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
+}
+add_action('widgets_init', 'remove_recent_comments_style'); 
---
+++ templates/footer.php
@@ -0,0 +1,25 @@
+<footer class="content-info">
+  <div class="container">
+   	<div class="row justify-content-center">
+		<div class="col-md-4 text-center">
+			<img src="/wp-content/uploads/2017/05/j-house-logo-big-grey.png" class="img-fluid">
+		</div>
+   	</div>
+  </div>
+  <hr>
+  <div class="container">
+	<div class="row">
+		<div class="col-md-12 text-center">
+		<ul class="footer-social-nav">
+			<li><i class="fa fa-facebook fa-2x"></i></li>
+			<li><i class="fa fa-twitter fa-2x"></i></li>
+			<li><i class="fa fa-linkedin fa-2x"></i></li>
+			<li><i class="fa fa-youtube fa-2x"></i></li>
+			<li><i class="fa fa-pinterest fa-2x"></i></li>
+			<li><i class="fa fa-instagram fa-2x"></i></li>
+		</ul>
+		</div>
+	</div>
+  </div>
+</footer>
+<div class="footer-bottom"></div>
