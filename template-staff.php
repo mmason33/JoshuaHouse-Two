fa1404cb3db8cbb82bfdeec899ed027a116d42e8
michael mason <michael@michaeljmason.com>
2017-08-22 13:21:33 -0700

    a couple style fixes

--- template-staff.php
+++ template-staff.php
@@ -7,27 +7,29 @@
 <div class="container">
   <div class="row staff-wrap">
     <div class="col-md-3">
-      <img src="/wp-content/uploads/2017/08/romi-300x300.jpg" class="img-fluid" />
+      <img src="/wp-content/uploads/2017/08/Albert-Martinez-1.jpg" class="img-fluid" />
     </div>
     <div class="col-md-9">
-      <h3>Rami Merhi</h3>
-      <h4 style="color:#666;">MA, LMFT, CADC, RAS </h4>
+      <h3>Albert Martinez</h3>
+      <h4 style="color:#666">Program Manager</h4>
+      <h5 style="color:#666;">CADC II</h5>
       <p>
-       Rami Merhi and I have been in the drug and alcohol field for 8 years. I went to school at National University and received a Masters in marriage family therapy, bachelors in psychology and a minor in Addictive disorders.  I am currently credentialed as Marriage Family Therapist intern, Certified Drug and Alcohol Counselor and Certified Addictions specialist. I have experience working in Detox,  Residential,Intensive outpatient and outpatient. I enjoy working with clients to overcome their struggles and make changes in there life to be successful. Working in this field is a passion for me I look forward to working with those in need for a better life.
+      Albert has been working in the field of mental health and substance abuse for over 11 years. His versatility in specializations spans across relapse prevention, self-esteem and anger management – whether a client needs assistance with self-identity, autonomy, coping skills or life skills, Albert is a qualified resource. He is a certified addiction counselor and is currently taking courses toward earning a bachelor’s degree in human services at the University of Phoenix. Atop all his professional success, Albert has gone out of the way to classify himself as a very happy father and grandfather who enjoys spending free time traveling the world with his life.
       </p>
     </div>
   </div>
   <hr />
   <div class="row staff-wrap">
-    <div class="col-md-9">
-      <h3>Albert Martinez</h3>
-      <h4 style="color:#666;">CADC II</h4>
-      <p>
-      Albert has been working in the field of mental health and substance abuse for over 11 years. His versatility in specializations spans across relapse prevention, self-esteem and anger management – whether a client needs assistance with self-identity, autonomy, coping skills or life skills, Albert is a qualified resource. He is a certified addiction counselor and is currently taking courses toward earning a bachelor’s degree in human services at the University of Phoenix. Atop all his professional success, Albert has gone out of the way to classify himself as a very happy father and grandfather who enjoys spending free time traveling the globe with his life.
-      </p>
-    </div>
     <div class="col-md-3">
-      <img src="/wp-content/uploads/2017/08/Albert-Martinez-1.jpg" class="img-fluid" />
+      <img src="/wp-content/uploads/2017/08/romi-300x300.jpg" class="img-fluid" />
+    </div>
+    <div class="col-md-9">
+      <h3>Rami Merhi</h3>
+      <h4 style="color:#666;">Primary Clinician</h4>
+      <h5 style="color:#666;">MA, LMFT, CADC, RAS </h5>
+      <p>
+      Rami has been in the drug and alcohol field for eight years since earning a master’s degree in marriage and family therapy from National University. Rami also holds a bachelor's degree in psychology and a minor degree in addictive disorders, along with credentials for marriage and family therapy and certification for both drug and alcohol counseling and addictive disorders. He has worked at every level of clinical recovery -- detox, residential, intensive outpatient (IOP) and outpatient. Clients determined to overcome their struggles with drug and alcohol abuse and transform their lives have connected well with Rami, who has listed his professional passion as working with individuals who have identified the need to improve their lives.
+      </p>
     </div>
   </div>
 </div>
--- templates/footer.php
+++ templates/footer.php
@@ -2,7 +2,7 @@
   <div class="container">
    	<div class="row justify-content-center">
 		<div class="col-md-4 text-center">
-			<img src="/wp-content/uploads/2017/05/j-house-logo-big-grey.png" class="img-fluid">
+			<img src="/wp-content/uploads/2017/08/j-house-logo-compressed-1.png" class="img-fluid">
       <hr />
       <h4><a href="tel:8663110003"><i class="fa fa-mobile"></i> 866.311.0003</a></h4>
 		</div>
